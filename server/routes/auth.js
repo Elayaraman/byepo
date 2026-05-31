5c1f2d953dd0bb02c80414cda16f384ba0acaf57
Elayaraman <elayaraman007@gmail.com>
2026-05-31 14:12:47 +0530

    router setup

---
+++ server/routes/auth.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const router = express.Router()
+
+router.post('/signup', (req, res) => {
+})
+
+router.post('/login', (req, res) => {
+})
+
+export default router
--- server/server.js
+++ server/server.js
@@ -1,31 +1,13 @@
 import express from 'express'
 import sqlite3 from 'sqlite3'
+import authRouter from './routes/auth.js'
 const app = express();
 const db = new (sqlite3.verbose().Database)("./database.db");
 const port = 3000;
 
-db.serialize(() => {
-  db.run('CREATE TABLE lorem (info TEXT)');
-  const stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+const PREFIX = '/_api/'
 
-  for (let i = 0; i < 10; i++) {
-    stmt.run(`Ipsum ${i}`);
-  }
-
-  stmt.finalize();
-
-  db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
-    if (err) {
-      console.error('Error:', err);
-      return;
-    }
-    console.log(`${row.id}: ${row.info}`);
-  });
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use((PREFIX+"auth"),authRouter)
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
