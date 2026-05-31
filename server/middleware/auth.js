c974ad2006815fdfce3cd3fa919b467c78b091f9
Elayaraman <elayaraman007@gmail.com>
2026-05-31 18:33:56 +0530

    role check methods

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 import { verifyToken } from "../services/auth.js";
 
-export default function  authMiddleware(req, res, next) {
+export default function authMiddleware(req, res, next) {
     console.log(req.headers.authorization);
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
@@ -11,5 +11,19 @@ export default function  authMiddleware(req, res, next) {
         return res.status(401).json({ success: false, error: 'Unauthorized: Invalid token' });
     }
     req.user = user;
+    next();
+}
+
+export function superAdminOnly(req, res, next) {
+    if (req.user.role !== 'super_admin') {
+        return res.status(403).json({ success: false, error: 'Forbidden: Super admin only' });
+    }
+    next();
+}
+
+export function orgAdminOnly(req, res, next) {
+    if (req.user.role !== 'org_admin') {
+        return res.status(403).json({ success: false, error: 'Forbidden: Org admin only' });
+    }
     next();
 }
